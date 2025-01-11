c35492d80e087c8a8dd3546fb2e060549bbe8324
softllorde <softlord.payne@yandex.ru>
2025-01-11 23:57:31 +0300

    -add print spaceship info in db_manager; -fix issues;

--- database_manager.py
+++ database_manager.py
@@ -5,10 +5,10 @@ from spaceship import Spaceship
 
 class Database_Manager:
 	def __init__(self, db_name):
-		self.conn = sqlite3.connect(f"db/{db_name}")
+		self.conn = sqlite3.connect(f"{db_name}")
 		self.cursor = self.conn.cursor()
 
-	def checkUserInDataBase(self,user_id):
+	def checkUserInDataBase(self, id):
 		query = "SELECT COUNT(*) FROM players WHERE id = ?"
 		self.cursor.execute(query, (id,))
 		result = self.cursor.fetchone()[0]
@@ -38,8 +38,16 @@ class Database_Manager:
 		self.cursor.execute("SELECT * FROM players WHERE id=?", (player_id,))
 		player_data = self.cursor.fetchone()
 		if player_data:
-			id,name,balance = player_data
+			id, name, balance = player_data
 			return player_data
+		else:
+			return None
+
+	def get_player_spaceship(self, id):
+		self.cursor.execute("SELECT * FROM player_spaceships WHERE player_id=?", (id,))
+		spaceship = self.cursor.fetchone()
+		if spaceship:
+			id, name, type, hold_capacity, attack, defence = spaceship
 		else:
 			return None
 
--- main.py
+++ main.py
@@ -8,7 +8,15 @@ bot = telebot.TeleBot('7192017546:AAHmm8rgHh2sR_wHTYMh0pqS_5NWUPJRPGc', skip_pen
 @bot.message_handler()
 def answer(message):
 	if message.text == "Привет":
-		db = Database_Manager()
+		db = Database_Manager("database.db")
 		bot.reply_to(message, db.add_user(message.from_user.id, message.from_user.first_name))
+
+	if message.text == "Инфо":
+		db = Database_Manager("database.db")
+		player_data = db.get_player_info(message.from_user.id)
+		id, name, balance = player_data
+		spaceship = db.get_player_spaceship(message.from_user.id)
+		spaceship_id, spaceship_name, spaceship_type, spaceship_hold_capacity, spaceship_attack, spaceship_defence = spaceship
+		bot.reply_to(message, f"Character:\n\nID: {id}\nName: {name}\nBalance: {balance}\n\nSpaceship:\n\nName: {spaceship_name}\nType: {spaceship_type}\nHold Capacity: {spaceship_hold_capacity}\nAttack: {spaceship_attack}\nDefence: {spaceship_defence}")
 
 bot.infinity_polling()
